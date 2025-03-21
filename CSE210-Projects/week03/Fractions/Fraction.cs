ab96a438f9867d93925a11aa85d87f1f10d76da9
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-21 17:13:24 +0100

    Encapsulation Activity

---
+++ CSE210-Projects/week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top= 1;
+        _bottom= 1;
+    }
+
+    public void PrintFraction()
+    {
+        Console.WriteLine($"{_top}/{_bottom}");
+    }
+
+    public Fraction(int top)
+    {
+        _top=top;
+        _bottom=1;
+    }
+     public Fraction(int top , int bottom)
+     {
+        _top=top;
+        _bottom=bottom;
+     }
+
+     public int GetTop()
+     {
+        return _top;
+     }
+
+     public void setTop( int top)
+     {
+        _top=top;
+     }
+
+     public int GetBottom()
+     {
+        return _bottom;
+     }
+
+     public void setBottom(int bottom)
+     {
+        _bottom=bottom;
+     }
+
+     public string GetFractionString()
+     {
+        return $"{_top}/{_bottom}";
+     }
+
+     public double GetDecimalValue()
+     {
+        return (double)_top/_bottom;
+     }
+}
--- CSE210-Projects/week03/Fractions/Program.cs
+++ CSE210-Projects/week03/Fractions/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1=new Fraction();
+        Fraction fraction2= new Fraction(3);
+        Fraction fraction3=new Fraction(3,7);
+
+        Console.WriteLine("Testing Constructors");
+        fraction1.PrintFraction();
+        fraction2.PrintFraction();
+        fraction3.PrintFraction();
+
+        //Using setters to set new values
+        fraction1.setTop(5);
+        fraction1.setBottom(6);
+        fraction2.setTop(4);
+        fraction2.setBottom(2);
+        fraction3.setTop(5);
+        fraction3.setBottom(7);
+        
+        //using getters to retrive the new values
+        Console.WriteLine("Updated values using setters:");
+        Console.WriteLine($"Fraction1: {fraction1.GetTop()}/{fraction1.GetBottom()}");
+        Console.WriteLine($"Fraction2: {fraction2.GetTop()}/{fraction2.GetBottom()}");
+        Console.WriteLine($"Fraction3: {fraction3.GetTop()}/{fraction3.GetBottom()}");
+
+        DisplayFractionalDetails(fraction1);
+        DisplayFractionalDetails(fraction2);
+        DisplayFractionalDetails(fraction3); 
+        
     }
+    public static void DisplayFractionalDetails(Fraction fraction)
+    {
+        Console.WriteLine($"Fraction {fraction.GetFractionString()}");
+        Console.WriteLine($"Decimal Value: {fraction.GetDecimalValue():0.00}");
+    }
+
 }
