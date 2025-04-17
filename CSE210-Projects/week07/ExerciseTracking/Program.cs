27f9fec04eb204ecc4e348a3f61666c2e359457a
mma200631 <assumpta.chidinma06@gmail.com>
2025-04-17 18:37:18 +0100

    final project

--- CSE210-Projects/week07/ExerciseTracking/Program.cs
+++ CSE210-Projects/week07/ExerciseTracking/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+       List<Activity> activities = new List<Activity>
+        {   new Running("03 nov 2022", 25, 14.5),
+            new Cycling("03 Nov 2022", 20, 12.0),
+            new Swimming("03 Nov 2022", 15, 40)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
     }
 }
